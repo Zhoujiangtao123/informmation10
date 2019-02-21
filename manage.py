3db1c8e684858b3060c9f92f1513d8d7499766b6
xwp <xwp_fullstack@163.com>
2019-02-21 10:37:38 +0800

    配置数据库

--- manage.py
+++ manage.py
@@ -1,6 +1,20 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+
+class Config(object):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/GitHup_project"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+app.config.from_object(Config)
+
+# 创建数据库对象
+db = SQLAlchemy(app)
+
+
 
 
 @app.route("/")
@@ -9,4 +23,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
